812a1f0881a830e999bc21bc5f75dd8cb0559d83
v.liparteliani <v.liparteliani@adjarabet.com>
2022-04-11 23:44:02 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const product = require('./api/product');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/product", product);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app;
